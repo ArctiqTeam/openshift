347777306a6097c6c3e40f6d84072f1aabc92cca
stewartshea <shea.stewart@arctiq.ca>
2016-06-17 10:45:44 -0400

    Updates by Cassius

--- backup-tools/project_export.sh
+++ backup-tools/project_export.sh
@@ -1,14 +1,34 @@
 #!/bin/sh
 ## Export all project data for reference and re-import
 ## created by Shea Stewart shea.stewart@arctiq.ca
-d=`date +%y%m%d%H%M%S`
-mkdir ~/ose_project_export_$d
-cd ~/ose_project_export_$d
-oc get project | awk 'FNR>1 {print $1}' > project_list
-for n in $(cat project_list); 
-    do 
-        oc project $n
-        oc export svc,sa,dc,bc,route,rc,is,templates -o yaml --exact > ${n}_exact.yaml
-        oc export svc,sa,dc,bc,route,rc,is,templates -o yaml --raw > ${n}_raw.yaml
-        oc export svc,sa,dc,bc,route,rc,is,templates -o yaml > ${n}_for_import.yaml
-    done
+## Modified by Cassius John-Adams cassius.john-adams@cbc.ca
+## to check for existing backup first, and perform cleanups
+fullDay=$(date +%Y%m%d)
+if [ ! -d ~/project_exports/ose_project_export_$fullDay ];
+    then
+    mkdir ~/project_exports/ose_project_export_$fullDay
+    cd ~/project_exports/ose_project_export_$fullDay
+    oc get project | awk 'FNR>1 {print $1}' > project_list
+    for proj in $(cat project_list); 
+        do 
+            oc project $proj
+            oc export svc,sa,dc,bc,route,rc,is,templates -o yaml --exact > ${proj}_exact.yaml
+            oc export svc,sa,dc,bc,route,rc,is,templates -o yaml --raw > ${proj}_raw.yaml
+            oc export svc,sa,dc,bc,route,rc,is,templates -o yaml > ${proj}_for_import.yaml
+        done
+else
+    echo "Nothing to do.  Project exports has already run once today"
+fi
+# Cleanup so we don't fill the root folder
+# Cleanup for 30 and 31 days ago, on the odd chance that 
+# this script didn't run yesterday.
+for delDay in 30 31;
+    do
+    cleanupDay=$(date "--date=${fullDay} -$delDay day" +%Y%m%d)
+    if [ ! -d ~/project_exports/ose_project_export_$cleanupDay ];
+            then
+            echo "Cleaning up ~/project_exports/ose_project_export_$cleanupDay"
+            rm -rf ~/project_exports/ose_project_export_$cleanupDay
+            echo "Cleanup complete"
+    fi
+done
