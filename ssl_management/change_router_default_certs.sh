bc2e0c0fb51141af69668ea0fbc66c261b3df61b
stewartshea <shea.stewart@arctiq.ca>
2017-08-31 19:43:53 -0400

    updated readme and notes

--- ssl_management/change_router_default_certs.sh
+++ ssl_management/change_router_default_certs.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-## This script is intended to replace the default SSL certificate assigned to the OpenShift router pods
-## This process can be peformed without service disruption, rather than re-deploying the routers (which has a service impact)
-## The SSL certificate contents are mounted as a secret inside of the router pods, which this script will replace and re-deploy
-## You must have cluster-admin privileges, and this assumes the router deployment config has the default name
+## This script is intended to replace the default SSL certificate assigned to the
+## OpenShift router pods. It will also add the default certificate into the
+## REGISTRY CONSOLE pod.
+
+## This process can be peformed without service disruption, rather than
+## re-deploying the routers (which has a service impact)
+
+## The SSL certificate contents are mounted as a secret inside of the router pods,
+## which this script will replace and re-deploy
+## You must have cluster-admin privileges, and this assumes the router deployment
+## config has the default name
+
 ## Pass the new certificate file names in this order: NEW_CERT NEW_PRIVATE_KEY NEW_CA_CERT
 ## ie. ./change_router_default_cert.sh apps_cert.crt apps_key.key ca_cert.crt
 
